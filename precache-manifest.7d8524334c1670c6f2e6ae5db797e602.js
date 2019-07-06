e1059dcd3a0515ab535dd2fb102cd23f66dcc4b7
zakwan93 <zakwan_bhaiyat@yahoo.com>
2019-07-06 01:55:15 -0400

    Updates

---
+++ precache-manifest.7d8524334c1670c6f2e6ae5db797e602.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1ede2a598fb6e482876de3cd553ef95",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "e659199a91298acb7069",
+    "url": "/monster-rolodex/static/css/main.61144021.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monster-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "e659199a91298acb7069",
+    "url": "/monster-rolodex/static/js/main.85b4ea88.chunk.js"
+  },
+  {
+    "revision": "344166cfeaf30c2159bd",
+    "url": "/monster-rolodex/static/js/runtime~main.5df944a8.js"
+  }
+]);
